0824c04c7b06f9922ef4910cf7a620a8e44821ed
日崎智博 <magatama.club@gmail.com>
2019-10-25 02:17:10 +0900

    no message

--- src/js/seminar_schedule.js
+++ src/js/seminar_schedule.js
@@ -3,5 +3,4 @@ import ReactDOM from "react-dom";
 import App from "./app";
 
 const target = document.getElementById("seminarScheduleTable");
-const jsonPath = target.getAttribute("data-json-path");
-ReactDOM.render(<App jsonPath={jsonPath} />, target);
+ReactDOM.render(<App />, target);
