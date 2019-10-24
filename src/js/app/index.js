3a85ee29859a9a11ed575cf82602b430b4de1a93
日崎智博 <magatama.club@gmail.com>
2019-10-25 02:02:27 +0900

    jsonじゃなくてjsにする。

--- src/js/app/index.js
+++ src/js/app/index.js
@@ -1,48 +1,65 @@
 import React, { useEffect, useState } from "react";
-import axios from "axios";
 import moment from "moment";
 import styled from "styled-components";
 import List from "./components/List";
 import { mq } from "./constants";
 
-const App = ({ jsonPath }) => {
+const App = () => {
   const [schedule, setSchedule] = useState(null);
   const [error, setError] = useState("");
 
   useEffect(() => {
-    axios
-      .get(jsonPath, { responseType: "json" })
-      .then(res => {
-        if (!Array.isArray(res.data)) {
-          setError("不正なデータです。");
-          return;
+    const scheduleData = window.____SEMINAR_SCHEDULE_DATA_ARRAY____ || null;
+
+    if (!scheduleData) {
+      setError("データがないか、不正です。");
+      return;
+    }
+
+    if (!Array.isArray(scheduleData)) {
+      setError("不正なデータです。");
+      return;
+    }
+
+    const dataForState = scheduleData.reduce((newData, day) => {
+      const month = moment(day.date).format("YYYYMM01");
+
+      // 今月
+      const currentMonth = moment().format("YYYYMM01");
+      // 翌月
+      const nextMonth = moment()
+        .add("months", 1)
+        .format("YYYYMM01");
+
+      // 表示させるのは今月と翌月の分だけ
+      if (month === currentMonth || month === nextMonth) {
+        if (!newData[month]) {
+          newData[month] = [day];
+        } else {
+          newData[month].push(day);
         }
+      }
 
-        const dataForState = res.data.reduce((newData, day) => {
-          const month = moment(day.date).format("YYYYMM01");
-          if (!newData[month]) {
-            newData[month] = [day];
-          } else {
-            newData[month].push(day);
-          }
-          return newData;
-        }, {});
+      return newData;
+    }, {});
 
-        setSchedule(dataForState);
-      })
-      .catch(e => {
-        setError(e.message);
-      });
+    setSchedule(dataForState);
   }, []);
 
   useEffect(() => {
     if (!!schedule) {
       const months = Object.keys(schedule);
-      const startMonthStr = moment(months[0]).format("YYYY年M月");
-      const endMonthStr = moment(months[months.length - 1]).format("YYYY年M月");
+      const startMonthStr =
+        months.length > 0 ? moment(months[0]).format("YYYY年M月") : "";
+      const endMonthStr =
+        months.length > 0
+          ? moment(months[months.length - 1]).format("YYYY年M月")
+          : "";
       const targetDom = document.getElementById("seminarPeriod");
       if (targetDom) {
-        targetDom.textContent = `${startMonthStr}～${endMonthStr}`;
+        targetDom.textContent = `${startMonthStr}${
+          startMonthStr !== endMonthStr ? `～${endMonthStr}` : ""
+        }`;
       }
     }
   }, [schedule]);
@@ -58,9 +75,13 @@ const App = ({ jsonPath }) => {
   return (
     <>
       <Root>
-        {Object.entries(schedule).map(([month, list]) => {
-          return <List key={month} month={month} list={list} />;
-        })}
+        {Object.entries(schedule).length > 0 &&
+          Object.entries(schedule).map(([month, list]) => (
+            <List key={month} month={month} list={list} />
+          ))}
+        {!Object.entries(schedule).length && (
+          <NoSchedule>開催予定はありません。</NoSchedule>
+        )}
       </Root>
       <Note>
         セミナー内容は、予告なく変更する場合があります。
@@ -91,6 +112,14 @@ const Error = styled.p`
   margin-top: 20px;
   color: red;
   font-size: 16px;
+`;
+
+const NoSchedule = styled.p`
+  margin-top: 20px;
+  color: #005bac;
+  font-size: 16px;
+  text-align: center;
+  flex: 0 0 100%;
 `;
 
 const Note = styled.p`
